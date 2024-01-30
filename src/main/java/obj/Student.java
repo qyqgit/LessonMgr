45f59d0720b4cb9c24cc07bf0c385f17d74f1651
qyqgit <qyqm@live.cn>
2024-01-30 18:23:58 +0800

    modify student class.

--- src/main/java/obj/Student.java
+++ src/main/java/obj/Student.java
@@ -1,5 +1,82 @@
 package obj;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class Student {
 	private String id;
+	private String name;
+	private String birthday;
+	private String sex;
+	private String date;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public Student(String id, String name, String birthday, String sex, String date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthday = birthday;
+		this.sex = sex;
+		this.date = date;
+	}
+	public static boolean getStudentList(Connection conn, ArrayList<Student> studentList) {
+		PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            pstmt = conn.prepareStatement("SELECT * FROM lesson.student");
+            rs = pstmt.executeQuery();
+            
+            while (rs.next()) {
+                Student student = new Student(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getString("password"),
+                        rs.getString("birthday"),
+                        rs.getString("sex"),
+                        rs.getString("date"),
+                        rs.getString("invalid")
+                        );
+                userList.add(user);
+            }
+            return true;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally {
+            Database.closeResultSet(rs);
+            Database.closePreparedStatement(pstmt);
+        }
+        return false;
+	}
 }
